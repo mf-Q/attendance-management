f8b54f47525923f2f58a69e13d7bbc105eb8989d
mf-Q <loud_flamingo@hotmail.com>
2020-01-05 00:22:19 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(name: user_params[:name], email: user_params[:email], password: user_params[:password])
     @user.save
-    redirect_to("/")
+    redirect_to("/users/index")
   end
 
   def edit
